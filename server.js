2111faeb63eef3f7aa03222f07edd712281ba79b
Joshua K Bader <jaybshinsen@hotmail.com>
2020-05-29 23:49:32 +0800

    Quick update
    Quick update to add in handlebars to server.js

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use(passport.session());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
