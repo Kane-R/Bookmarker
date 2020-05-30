7384777f51ab761f37944a1a7a176868f45b6759
Roz <roz_E4@hotmail.com>
2020-05-30 10:59:39 +0800

    Auto stash before rebase of "origin/connection"

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,47 @@
+<<<<<<< refs/remotes/origin/connection
+DROP DATABASE IF EXISTS bookmarkr_db;
+CREATE DATABASE bookmarkr_db;
+USE bookmarkr_db;
+CREATE TABLE users
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+CREATE TABLE bookmarks
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    url VARCHAR(255) NOT NULL,
+    user_id int not null,
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    click_count INT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tags
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+-- 
+CREATE TABLE bookmark_tag
+(
+	tag_id INT NOT NULL,
+    bookmark_id INT NOT NULL,
+    CONSTRAINT FK_tag_id FOREIGN KEY (tag_id)
+        REFERENCES tags (id),
+    CONSTRAINT FK_bookmark_id FOREIGN KEY (bookmark_id)
+        REFERENCES bookmarks (id),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+=======
 --create tables--
 
 -- CREATE DATABASE bookmarkr_db;
@@ -42,3 +86,4 @@
 --         REFERENCES bookmarks (id),
 --     createdAt TIMESTAMP NOT NULL
 -- );
+>>>>>>> Auto stash before merge of "Roz" and "origin/Roz"
