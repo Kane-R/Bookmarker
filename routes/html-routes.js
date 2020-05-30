32a109d4905f505e35e1425861de33afd0934619
Joshua K Bader <jaybshinsen@hotmail.com>
2020-05-30 11:39:04 +0800

    clean up
    cleaned up will branch from here

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,44 +3,44 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', function (req, res) {
-        res.render('index', {layout: 'main' })
-      }); 
+    res.render('index', {
+        layout: 'main'
+    })
+});
 
 
-    // Main Page - includes sign up and login button
-   // app.get('/', function(req, res) {
-   //     if (req.user) {
-  //          res.redirect("/members");
-  //        }
-  //        //set to members while developing - remember to change this
-   //       res.sendFile(path.join(__dirname, "../public/member.html"));
-   // });
+// Main Page - includes sign up and login button
+// app.get('/', function(req, res) {
+//     if (req.user) {
+//          res.redirect("/members");
+//        }
+//        //set to members while developing - remember to change this
+//       res.sendFile(path.join(__dirname, "../public/member.html"));
+// });
 
 
-    // Login page
+// Login page
 
-    app.get('/login', function(req, res) {
-        if (req.user) {
-            res.redirect("/members");
-          }
-          //set to members while developing - remember to change this
-          res.sendFile(path.join(__dirname, "../public/login.html"));
-    });
+app.get('/login', function (req, res) {
+    if (req.user) {
+        res.redirect("/members");
+    }
+    //set to members while developing - remember to change this
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+});
 
 
-    // Members page (dashboard)
+// Members page (dashboard)
 
-    app.get('/dashboard', function(req, res) {
-        if (req.user) {
-            res.redirect("/members");
-          }
-          //set to members while developing - remember to change this
-          res.sendFile(path.join(__dirname, "../public/member.html"));
-    });
+app.get('/dashboard', function (req, res) {
+    if (req.user) {
+        res.redirect("/members");
+    }
+    //set to members while developing - remember to change this
+    res.sendFile(path.join(__dirname, "../public/member.html"));
+});
 
-    // 
+// 
 
 
 module.exports = router;
-
-
