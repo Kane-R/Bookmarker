30124ae59026f4f3964b75a5557462d03bb175a4
Roz <roz_E4@hotmail.com>
2020-05-30 10:05:58 +0800

    Auto stash before merge of "Roz" and "origin/Roz"

--- db/schema.sql
+++ db/schema.sql
@@ -1,43 +1,44 @@
 --create tables--
 
-CREATE DATABASE bookmarkr_db;
+-- CREATE DATABASE bookmarkr_db;
 
-USE bookmarkr_db;
+-- USE bookmarkr_db;
 
-CREATE TABLE users
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255),
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE users
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL,
+--     password VARCHAR(255),
+--     oauthToken VARCHAR(255),
+--     createdAt TIMESTAMP NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE bookmarks
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    url VARCHAR(255) NOT NULL,
-    CONSTRAINT FK_user_id FOREIGN KEY (user_id)
-        REFERENCES users (id),
-    click_count INT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE bookmarks
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     url VARCHAR(255) NOT NULL,
+--     CONSTRAINT FK_user_id FOREIGN KEY (user_id)
+--         REFERENCES users (id),
+--     click_count INT NULL,
+--     createdAt TIMESTAMP NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE tags
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE tags
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(255) NOT NULL,
+--     createdAt TIMESTAMP NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE bookmark_tag
-(
-    CONSTRAINT FK_tag_id FOREIGN KEY (tag_id)
-        REFERENCES tags (id),
-    CONSTRAINT FK_bookmark_id FOREIGN KEY (bookmark_id)
-        REFERENCES bookmarks (id),
-    createdAt TIMESTAMP NOT NULL
-);
+-- CREATE TABLE bookmark_tag
+-- (
+--     CONSTRAINT FK_tag_id FOREIGN KEY (tag_id)
+--         REFERENCES tags (id),
+--     CONSTRAINT FK_bookmark_id FOREIGN KEY (bookmark_id)
+--         REFERENCES bookmarks (id),
+--     createdAt TIMESTAMP NOT NULL
+-- );
