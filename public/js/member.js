73b6a24f7d1869536823045120a1932e631f1bbd
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-01 04:32:26 +0800

    CssUpdates
    Updated Css, Ensured all buttons are labelled and added log out pop up

---
+++ public/js/member.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $("#potrait").click(function (event) {
+        event.preventDefault();
+        $("#user-dropdown").toggleClass("hidden")
+    })
+
+
+
+
+
+
+});
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      'Permanent Marker': ["'Permanent Marker'", "cursive"],
+    },
     extend: {},
   },
   variants: {},
