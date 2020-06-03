c39964dfb6c0c0788d35493617fb0683caa85757
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-03 14:54:59 +0800

    Auto stash before rebase of "origin/Roz"

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,19 @@
 // Requiring our models and passport as we've configured it
 
 const db = require("../models");
-const router = express.Router();
 
 module.exports = function(app) {
 
-  app.post("/bookmarks"), function (req,res) {
+  // POST route for saving a new bookmark
+    app.post("/bookmarks", function(req, res) {
       console.log(req.body);
-        // db.Post.create({
-        //   title: req.body.title,
-        //   body: req.body.body,
-        //   category: req.body.category
-        // })
-        //   .then(function(dbPost) {
-        //     res.json(dbPost);
-        //   });
-    };
-
-}
+      console.log(req.body.url);
+      db.Bookmark.create({
+        url: req.body.url,
+        userID: req.body.userID
+      })
+        .then(function(dbPost) {
+          res.json(dbPost);
+        });
+    });
+  }
