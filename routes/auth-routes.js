bb2360832a25117891182c5097d2e4f463a49bf9
Roz <roz_E4@hotmail.com>
2020-06-03 22:36:25 +0800

    go from login to members page.

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -19,7 +19,9 @@ router.get('/google', passport.authenticate('google',{
 
 // callback route for google to redirect to 
 router.get('/google/redirect', passport.authenticate('google'),(req,res) => {
-    res.send('you reached the callback URI')
+    console.log("authroutes");
+    
+    res.redirect('/members')
 });
 
 module.exports = router;
